830758dab585e6730dff43245bf9d4c2d0979ab8
fuzzymeme <fuzzymeme@gmail.com>
2017-03-28 16:58:56 +0100

    Added first test case

--- com/fuzzymeme/changecalculator/ChangeCalculator.java
+++ com/fuzzymeme/changecalculator/ChangeCalculator.java
@@ -1,7 +1,37 @@
 package com.fuzzymeme.changecalculator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ChangeCalculator {
 	
+	
+	public List<Map<Integer, Integer>> calculateCoinOptions(int target) {
+
+		List<Map<Integer, Integer>> expected = new ArrayList<>();
+		Map<Integer, Integer> one = new HashMap<>();
+		one.put(1, 11);
+		expected.add(one);
+
+		Map<Integer, Integer> two = new HashMap<>();
+		two.put(1, 6);
+		two.put(5, 1);
+		expected.add(two);
+
+		Map<Integer, Integer> three = new HashMap<>();
+		three.put(1, 1);
+		three.put(5, 2);
+		expected.add(three);
+
+		Map<Integer, Integer> four = new HashMap<>();
+		four.put(1, 1);
+		four.put(10, 1);
+		expected.add(four);
+		
+		return expected;
+	}
 	
 	public static void main(String[] args) {
 		
--- com/fuzzymeme/changecalculator/ChangeCalculatorTests.java
+++ com/fuzzymeme/changecalculator/ChangeCalculatorTests.java
@@ -2,13 +2,53 @@ package com.fuzzymeme.changecalculator;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class ChangeCalculatorTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_Given11_returns4ItemListWithCorrectEntries() {
+
+		List<Map<Integer, Integer>> expected = new ArrayList<>();
+		Map<Integer, Integer> one = new HashMap<>();
+		one.put(1, 11);
+		expected.add(one);
+
+		Map<Integer, Integer> two = new HashMap<>();
+		two.put(1, 6);
+		two.put(5, 1);
+		expected.add(two);
+
+		Map<Integer, Integer> three = new HashMap<>();
+		three.put(1, 1);
+		three.put(5, 2);
+		expected.add(three);
+
+		Map<Integer, Integer> four = new HashMap<>();
+		four.put(1, 1);
+		four.put(10, 1);
+		expected.add(four);
+
+		ChangeCalculator changer = new ChangeCalculator();
+		List<Map<Integer, Integer>> calculated = changer.calculateCoinOptions(11);
+
+		assertTrue(equivalentLists(calculated, expected));
+	}
+
+	//Helpers
+	private boolean equivalentLists(List<Map<Integer, Integer>> one, List<Map<Integer, Integer>> other) {
+
+		Set<Map<Integer, Integer>> oneSet = new HashSet<>(one);
+		Set<Map<Integer, Integer>> otherSet = new HashSet<>(other);
+
+		return oneSet.equals(otherSet);
 	}
 
 }
