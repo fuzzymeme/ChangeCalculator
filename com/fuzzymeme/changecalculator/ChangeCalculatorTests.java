e00e1838fffa4a6c3b2ee3d3b7e67bbfdf301e07
fuzzymeme <fuzzymeme@gmail.com>
2017-03-31 16:40:09 +0100

    Added a setup function and little clean-ups

--- com/fuzzymeme/changecalculator/ChangeCalculatorTests.java
+++ com/fuzzymeme/changecalculator/ChangeCalculatorTests.java
@@ -9,20 +9,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ChangeCalculatorTests {
 
+	private ChangeCalculator changer;
+	private List<Map<Integer, Integer>> expected = new ArrayList<>();
+	
+	@Before
+	public void setUp() {
+		changer = new ChangeCalculator();
+	}
+	
 	@Test
 	public void test_Given11_returns4ItemListWithCorrectEntries() {
 
-		List<Map<Integer, Integer>> expected = new ArrayList<>();
+		expected.clear();
 		expected.add(buildMap("1:11"));
 		expected.add(buildMap("1:6 5:1"));
 		expected.add(buildMap("1:1 5:2"));
 		expected.add(buildMap("1:1 10:1"));
 
-		ChangeCalculator changer = new ChangeCalculator();
 		List<Map<Integer, Integer>> calculated = changer.calculateCoinOptions(11);
 
 		assertFailIfNotEquivalentLists(calculated, expected);
@@ -31,7 +39,7 @@ public class ChangeCalculatorTests {
 	@Test
 	public void test_Given16_returns6ItemListWithCorrectEntries() {
 
-		List<Map<Integer, Integer>> expected = new ArrayList<>();
+		expected.clear();
 		expected.add(buildMap("1:16"));
 		expected.add(buildMap("1:6 10:1"));
 		expected.add(buildMap("1:1 5:3"));
@@ -39,7 +47,6 @@ public class ChangeCalculatorTests {
 		expected.add(buildMap("1:6 5:2"));
 		expected.add(buildMap("1:1 5:1 10:1"));
 
-		ChangeCalculator changer = new ChangeCalculator();
 		List<Map<Integer, Integer>> calculated = changer.calculateCoinOptions(16);
 
 		assertFailIfNotEquivalentLists(calculated, expected);
@@ -48,7 +55,7 @@ public class ChangeCalculatorTests {
 	@Test
 	public void test_Given23_returns9ItemListWithCorrectEntries() {
 
-		List<Map<Integer, Integer>> expected = new ArrayList<>();
+		expected.clear();
 		expected.add(buildMap("1:13 5:2"));
 		expected.add(buildMap("1:3 5:4"));
 		expected.add(buildMap("1:3 5:2 10:1"));
@@ -59,7 +66,6 @@ public class ChangeCalculatorTests {
 		expected.add(buildMap("1:3 10:2"));
 		expected.add(buildMap("1:8 5:3"));
 
-		ChangeCalculator changer = new ChangeCalculator();
 		List<Map<Integer, Integer>> calculated = changer.calculateCoinOptions(23);
 
 		assertFailIfNotEquivalentLists(calculated, expected);
@@ -69,8 +75,6 @@ public class ChangeCalculatorTests {
 	public void test_Given0_returns0ItemList() {
 
 		List<Map<Integer, Integer>> expected = new ArrayList<>();
-
-		ChangeCalculator changer = new ChangeCalculator();
 		List<Map<Integer, Integer>> calculated = changer.calculateCoinOptions(0);
 
 		assertFailIfNotEquivalentLists(calculated, expected);
@@ -85,7 +89,10 @@ public class ChangeCalculatorTests {
 		Set<Map<Integer, Integer>> otherSet = new HashSet<>(other);
 		
 		if(oneSet.size() != otherSet.size()) {
-			System.out.println("Two lists are different sizes " + oneSet.size() + " and " + otherSet.size());
+			System.out.println("Two lists are different sizes " + 
+					oneSet.size() + 
+					" and " + 
+					otherSet.size());
 		}
 		
 		if(!oneSet.equals(otherSet)) {
@@ -107,7 +114,8 @@ public class ChangeCalculatorTests {
 		String[] items = mapDefinition.split(" ");
 		for(String item: items) {
 			String[] entry = item.split(":");
-			result.put(Integer.parseInt(entry[0].trim()), Integer.parseInt(entry[1].trim()));
+			result.put(Integer.parseInt(entry[0].trim()), 
+					   Integer.parseInt(entry[1].trim()));
 		}
 		
 		return result;
